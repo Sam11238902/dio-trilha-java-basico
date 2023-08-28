0e772251ccccacf5f7be7b8ec940e78cc256d435
Samuel Holanda <samuel112928@gmail.com>
2023-08-28 16:12:15 -0300

    atualizando IfElse

--- estrutura.condicional.repeticao/IfElse.java
+++ estrutura.condicional.repeticao/IfElse.java
@@ -1,6 +1,15 @@
 
 public class IfElse {
 	public static void main(String[] args) {
-		System.out.println("estrutura if e else");
+		double saldo = 25.0;
+		double valorSolicitado = 17.0;
+		if (valorSolicitado < saldo) {
+			saldo = saldo - valorSolicitado;
+			System.out.println("Saque efetuado !");
+		} else {
+			
+			System.out.println("Sem saldo suficiente !");
+
+		}
 	}
 }
